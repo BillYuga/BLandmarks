660b4eec6a4338740252b283920abd4c46fb4ac4
Bill Fosam <billyuga37@gmail.com>
2023-11-02 01:05:57 -0500

    Initial Commit

---
+++ BLandmarks/BLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLandmarksApp.swift
+//  BLandmarks
+//
+//  Created by Bill  on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
